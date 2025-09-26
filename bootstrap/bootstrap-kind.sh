d8acd5fbb8c9f940d28f852156a75444ccf91b96
tjololo <1145298+tjololo@users.noreply.github.com>
2025-09-26 16:48:01 +0200

    add bootstrap and update gloo version

---
+++ bootstrap/bootstrap-kind.sh
@@ -0,0 +1,21 @@
+cat <<EOF | kind create cluster --config=-
+kind: Cluster
+apiVersion: kind.x-k8s.io/v1alpha4
+nodes:
+- role: control-plane
+  extraPortMappings:
+  - containerPort: 8080
+    hostPort: 8080
+    protocol: TCP
+EOF
+
+kubectl config use-context kind-kind
+
+flux install
+
+kubectl wait --for=condition=available --timeout=90s deployment -n flux-system source-controller
+kubectl wait --for=condition=available --timeout=90s deployment -n flux-system kustomize-controller
+kubectl wait --for=condition=available --timeout=90s deployment -n flux-system helm-controller
+kubectl wait --for=condition=available --timeout=90s deployment -n flux-system notification-controller
+
+kubectl apply -f ./base-kustomize
