2cfca59a698940faa6fe91b4b189869ca47add58
tjololo <1145298+tjololo@users.noreply.github.com>
2025-12-05 16:31:07 +0100

    fix domain

--- bootstrap/bootstrap-kind.sh
+++ bootstrap/bootstrap-kind.sh
@@ -1,6 +1,6 @@
 cleanup() {
   echo "Removing entry from hosts file"
-  sudo bash -c "sed -i '' '/whoami\.418\.local/d' /etc/hosts"
+  sudo bash -c "sed -i '' '/kind\.418\.local/d' /etc/hosts"
   echo "Deleting kind cluster"
   kind delete cluster
   echo "Killing cloud-provider-kind with PID $1"
@@ -10,27 +10,19 @@ cleanup() {
 sudo -v
 # Ensure current kubectl context is not pointing to any cluster
 kubectl config unset current-context
-# cat <<EOF | KIND_EXPERIMENTAL_PROVIDER=podman kind create cluster --config=-
-# kind: Cluster
-# apiVersion: kind.x-k8s.io/v1alpha4
-# nodes:
-# - role: control-plane
-#   extraPortMappings:
-#   - containerPort: 30000
-#     hostPort: 8080
-#     protocol: TCP
-#   - containerPort: 30001
-#     hostPort: 8443
-#     protocol: TCP
-#   - containerPort: 9000
-#     hostPort: 9000
-#     protocol: TCP
-# EOF
 export KIND_EXPERIMENTAL_PROVIDER=podman
-kind create cluster
+cat <<EOF | kind create cluster --config=-
+kind: Cluster
+apiVersion: kind.x-k8s.io/v1alpha4
+nodes:
+- role: control-plane
+- role: worker
+- role: worker
+- role: worker
+EOF
 
 kubectl config use-context kind-kind
-kubectl label node kind-control-plane node.kubernetes.io/exclude-from-external-load-balancers-
+# kubectl label node kind-control-plane node.kubernetes.io/exclude-from-external-load-balancers-
 echo "Starting cloud-provider-kind"
 sudo cloud-provider-kind 2>/dev/null &
 CPK_PID=$!
@@ -50,8 +42,8 @@ kubectl wait --for=jsonpath='{.status.loadBalancer.ingress[0].ip}' -n traefik-sy
 
 SVC_IP=$(kubectl get service -n traefik-system traefik -o=jsonpath='{.status.loadBalancer.ingress[0].ip}')
 sudo sh -c 'cat <<EOF >> /etc/hosts
-# Kind cluster whoami.418.local
-'$SVC_IP' whoami.418.local
+# Kind cluster kind.418.local
+'$SVC_IP' kind.418.local
 EOF'
 trap "cleanup '$CPK_PID' '$SVC_IP'" EXIT INT TERM
 
